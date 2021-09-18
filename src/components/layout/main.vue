ec73c3adba10b62a7c02b90d2ea3c266968281da
delimond <gastondelimond@gmail.com>
2021-09-18 16:31:01 +0100

    latest commit

--- src/components/layout/main.vue
+++
@@ -1,8 +0,0 @@
-<template>
-  <!-- Content Wrapper. Contains page content -->
-  <div class="main-wrapper main-wrapper-1">
-    <div class="navbar-bg"></div>
-    <router-view></router-view>
-  </div>
-  <!-- /.content-wrapper -->
-</template>
